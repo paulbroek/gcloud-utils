4b8d102b0650ba0339c2d2343c2c95e7cdd7d9fb
paulbroek <pauliusbroek@hotmail.com>
2022-05-17 19:29:06 +0200

    apply python-black to bq_extract.py

--- utils/bq_extract.py
+++ utils/bq_extract.py
@@ -14,20 +14,20 @@
     or add the environment variable to ~/.bashrc or ~/.zshrc
 """
 
-from typing import Optional # , Union, Tuple, List
 import argparse
 import logging
 from datetime import datetime
+from typing import Optional  # , Union, Tuple, List
 
 import pandas as pd
-
 from google.cloud import bigquery
+from rarc_utils.misc import unnest_assign_cols  # ,unnest_dict
 
-from rarc_utils.misc import unnest_assign_cols # ,unnest_dict
+logger = logging.getLogger(__name__)  # 'root' 'main'
 
-logger = logging.getLogger(__name__) # 'root' 'main'
-
-BILLING_TABLE_NAME = 'BILLING_TABLE_NAME'
+BILLING_TABLE_NAME = (
+    "BILLING_TABLE_NAME"
+)
 
 
 def query_stackoverflow(cl):
@@ -47,6 +47,7 @@ def query_stackoverflow(cl):
 
     return query_job.result()  # Waits for job to complete.
 
+
 # @timet
 def query_billing_all(cl):
 
@@ -60,8 +61,9 @@ def query_billing_all(cl):
 
     return query_job.result()
 
+
 # @timet
-def query_billing(cl, cols='*', orderBy='export_time', n=100_000):
+def query_billing(cl, cols="*", orderBy="export_time", n=100_000):
 
     assert isinstance(cols, str)
     assert isinstance(n, int)
@@ -72,14 +74,19 @@ def query_billing(cl, cols='*', orderBy='export_time', n=100_000):
         FROM {}
         ORDER BY {} ASC
         LIMIT {}
-        """.format(cols, BILLING_TABLE_NAME, orderBy, n)
+        """.format(
+            cols, BILLING_TABLE_NAME, orderBy, n
+        )
     )
 
     return query_job.result()
 
+
 # @timet
-def query_billing_nonzero(cl, cols='*', orderBy='export_time', fromDate=datetime(2016,1,1), n=100_000):
-    """ query all nonzero billing rows """
+def query_billing_nonzero(
+    cl, cols="*", orderBy="export_time", fromDate=datetime(2016, 1, 1), n=100_000
+):
+    """query all nonzero billing rows"""
 
     assert isinstance(cols, str)
     assert isinstance(n, int)
@@ -93,13 +100,16 @@ def query_billing_nonzero(cl, cols='*', orderBy='export_time', fromDate=datetime
         WHERE ({} >= TIMESTAMP('{}')) AND (cost > 0)
         ORDER BY {} ASC
         LIMIT {}
-        """.format(cols, BILLING_TABLE_NAME, orderBy, sqlFromDate, orderBy, n)
+        """.format(
+            cols, BILLING_TABLE_NAME, orderBy, sqlFromDate, orderBy, n
+        )
     )
 
     return query_job.result()
 
+
 # @timet
-def to_pandas(res, ixCol: Optional='export_time') -> pd.DataFrame:
+def to_pandas(res, ixCol: Optional = "export_time") -> pd.DataFrame:
 
     df = res.to_dataframe()
 
@@ -111,125 +121,133 @@ def to_pandas(res, ixCol: Optional='export_time') -> pd.DataFrame:
 
     return df
 
+
 def cost_by_month(df) -> pd.DataFrame:
 
     raise NotImplementedError
     # return df
 
+
 def reduce_view(df) -> pd.DataFrame:
-    """ return a compact view of df, since BigQuery returns 19 columns and nested dictionaries """
+    """return a compact view of df, since BigQuery returns 19 columns and nested dictionaries"""
 
     df = df.copy()
 
-    df['usage_duration'] = df['usage_end_time'] - df['usage_start_time']
-    df['usage_secs'] = df['usage_duration'].dt.total_seconds()
-    df['usage_hours'] = df['usage_secs'] / 3600
+    df["usage_duration"] = df["usage_end_time"] - df["usage_start_time"]
+    df["usage_secs"] = df["usage_duration"].dt.total_seconds()
+    df["usage_hours"] = df["usage_secs"] / 3600
 
-    df['cumCost'] = df['cost'].cumsum()
+    df["cumCost"] = df["cost"].cumsum()
 
     # several ways to create nested columns, see unnest_dict()
     # df['project_id'] = df['project'].map(lambda x: x['id'])
     # df['project_id'] = unnest_dict(df, 'project', 'id', assign=False)
 
-    unnestList = [('project','id'), ('service','description'), ('sku','description'), ]
+    unnestList = [
+        ("project", "id"),
+        ("service", "description"),
+        ("sku", "description"),
+    ]
 
     # apply unnest_dict to al parameters in unnestList
     addedCols, df = unnest_assign_cols(df, unnestList)
 
     # what cols to keep
-    keepCols = addedCols + ['usage_hours', 'cost', 'cumCost']
+    keepCols = addedCols + ["usage_hours", "cost", "cumCost"]
     # view = df.loc[:, ['project_id', 'usage_hours','cost']]
     view = df.loc[:, keepCols]
 
     return view
 
-class ArgParser():
-    """ create CLI parser """
+
+class ArgParser:
+    """create CLI parser"""
 
     @staticmethod
     def get_parser():
 
-        CLI=argparse.ArgumentParser()
+        CLI = argparse.ArgumentParser()
         CLI.add_argument(
-          "-v", "--verbosity", 
-          type=str,         
-          default='info',
-          help="choose debug log level: DEBUG, INFO, WARNING, ERROR, CRITICAL"
+            "-v",
+            "--verbosity",
+            type=str,
+            default="info",
+            help="choose debug log level: DEBUG, INFO, WARNING, ERROR, CRITICAL",
         )
         CLI.add_argument(
-          "-f", "--file", 
-          action='store_true',         
-          default=False,
-          help="save log to file"
+            "-f", "--file", action="store_true", default=False, help="save log to file"
         )
 
         return CLI
 
 
 if __name__ == "__main__":
-    
+
     # from rarc.utils.decorators import timeit, timet
     from rarc.utils.log import setup_logger
 
-    parser      = ArgParser.get_parser()
-    args        = parser.parse_args()
+    parser = ArgParser.get_parser()
+    args = parser.parse_args()
 
-    log_fmt     = "%(asctime)s - %(module)-16s - %(lineno)-4s - %(funcName)-16s - %(levelname)-7s - %(message)s"  #name
-    log_level   = getattr(logging, args.verbosity.upper())
-    logger      = setup_logger(cmdLevel=log_level, saveFile=args.file, savePandas=1, fmt=log_fmt, multiLine=1)
+    log_fmt = "%(asctime)s - %(module)-16s - %(lineno)-4s - %(funcName)-16s - %(levelname)-7s - %(message)s"  # name
+    log_level = getattr(logging, args.verbosity.upper())
+    logger = setup_logger(
+        cmdLevel=log_level, saveFile=args.file, savePandas=1, fmt=log_fmt, multiLine=1
+    )
 
-
-    client = bigquery.Client() # works only when runing export=GOOGLE_APP.. before running python
+    client = (
+        bigquery.Client()
+    )  # works only when runing export=GOOGLE_APP.. before running python
     # client = bigquery.Client(credentials=os.environ['GOOGLE_APPLICATION_CREDENTIALS']) # works with docker-compose environment var
 
     # ress = query_billing(client, cols='*', n=100_000)
-    ress = query_billing_nonzero(client, cols='*', orderBy='usage_end_time', n=100_000)
+    ress = query_billing_nonzero(client, cols="*", orderBy="usage_end_time", n=100_000)
     # ress = query_billing_nonzero(client, cols='*', orderBy='export_time', n=100_000)
     # ress = query_billing_nonzero(client, cols='*', orderBy='cost', n=100_000)
     # ress = query_billing(client, cols='export_time, cost', n=100_000)
 
     # df = to_pandas(ress, ixCol='export_time')
-    df = to_pandas(ress, ixCol='usage_end_time')
+    df = to_pandas(ress, ixCol="usage_end_time")
 
     total_cost = df.cost.sum()
 
     nrow = len(df)
-    NROW = 'nrow' if nrow == 1 else 'nrows'
+    NROW = "nrow" if nrow == 1 else "nrows"
 
-    logger.info(f'got {nrow} {NROW}')
-    logger.info(f'total cost is {total_cost:.2f} $')
+    logger.info(f"got {nrow} {NROW}")
+    logger.info(f"total cost is {total_cost:.2f} $")
 
     view = reduce_view(df)
     # view = view.sort_values('cost')
     view = view.sort_index()
 
     # groupby day, to get cost per day
-    df['dt'] = df.index
-    by_day = df.groupby(df.dt.dt.floor('d'))
-    by_day['cost'].sum()
+    df["dt"] = df.index
+    by_day = df.groupby(df.dt.dt.floor("d"))
+    by_day["cost"].sum()
 
     # get a summary of top (summed) costs
-    by_service = view.groupby('service_description')
-    by_description = view.groupby('sku_description')
+    by_service = view.groupby("service_description")
+    by_description = view.groupby("sku_description")
     # costs = by_description['cost'].sum().sort_values()
-    costs = by_description.agg({'usage_hours': ['sum', 'max'], 'cost': 'sum'})
-    costs = costs.sort_values(('cost','sum'))
+    costs = by_description.agg({"usage_hours": ["sum", "max"], "cost": "sum"})
+    costs = costs.sort_values(("cost", "sum"))
     # optional flattening of column names
-    costs.columns = costs.columns.map('_'.join).str.strip('_')
+    costs.columns = costs.columns.map("_".join).str.strip("_")
 
     # display floats, not scientific numbers
-    pd.set_option('display.float_format', lambda x: '%.5f' % x)
-    costs['cost_sum_pct'] = costs['cost_sum'] / costs['cost_sum'].sum()
+    pd.set_option("display.float_format", lambda x: "%.5f" % x)
+    costs["cost_sum_pct"] = costs["cost_sum"] / costs["cost_sum"].sum()
 
     # usgcols = ['usage_hours_sum', 'usage_hours_max']
-    usgcols = costs.filter(regex='^usage.+').columns
+    usgcols = costs.filter(regex="^usage.+").columns
     costs[usgcols] = costs[usgcols].astype(int)
 
     # create cols with stars that visualize the pct
-    costs['cost_pct'] = '*' # choose any char
+    costs["cost_pct"] = "*"  # choose any char
     star_width = 10
     nstar = pd.cut(costs.cost_sum_pct, star_width, labels=range(star_width))
-    costs['cost_pct'] = costs['cost_pct'].str.repeat(nstar)
+    costs["cost_pct"] = costs["cost_pct"].str.repeat(nstar)
 
-    with pd.option_context('display.colheader_justify','left'):
-        print('\n', costs)
+    with pd.option_context("display.colheader_justify", "left"):
+        print("\n", costs)
